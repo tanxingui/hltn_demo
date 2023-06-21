cbbebaa1ed4272cdb907b8d7518653f1ea5ec664
TanXinGui <921017863@qq.com>
2023-06-21 10:18:39 +0800

    新增魔力老学员

--- API/Student.py
+++ API/Student.py
@@ -16,10 +16,10 @@ Thread_num = 200
 db = pymysql.connect(host="rm-2ze9tjvu49805vxbw.mysql.rds.aliyuncs.com", user="mmtest", password="Mmears2023",
                      database="db_support")
 cursor = db.cursor()
-sql = "SELECT soc.student_id FROM `student_ocean` so LEFT JOIN student_ocean_clt soc ON so.student_id = soc.student_id WHERE so.original_service_subject = 0 ORDER BY soc.student_id DESC limit 300;"
+sql = "SELECT soc.student_id FROM `student_ocean` so LEFT JOIN student_ocean_clt soc ON so.student_id = soc.student_id WHERE so.original_service_subject = 0 ORDER BY soc.student_id DESC limit 200;"
 cursor.execute(sql)
 results = [row[0] for row in cursor.fetchall()]
-token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzI1NiIsIm5hbWUiOiLosK3mlrDotLUiLCJzdXBwb3J0SWQiOm51bGwsImV4cCI6MTY4NzMxNTE2MiwiaWF0IjoxNjg3MjI4NzYyLCJlbWFpbCI6InRhbnhpbmd1aUBobHRuLmNvbSJ9.KuBTkr83K6lwas9qVGozlwI69b9c0bkj4Q_HFHcEorZTbfrVXAIwtyRVP9FFqZLSCIiPyA-FBw6D2lN1afHOkw'
+token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxMzI1NiIsIm5hbWUiOiLosK3mlrDotLUiLCJzdXBwb3J0SWQiOm51bGwsImV4cCI6MTY4NzM5OTUxMiwiaWF0IjoxNjg3MzEzMTEyLCJlbWFpbCI6InRhbnhpbmd1aUBobHRuLmNvbSJ9.z63PFP7vKZLiyF3xBi4CbB5Zfwu9FMpiO1NZd3b_8_Dv2mRgjzotF5FbNYUBfxpHOyU0XQZRDpQm139zByLnAg'
 new_list = []
 lock = threading.Lock()
 
@@ -32,16 +32,16 @@ def worker(q: Queue):
             'x-app-id': '2',
             'x-auth-token': token
         }
-        data = {"keyword": student_id}
+        form_data = {"keyword": student_id}
         try:
-            response = requests.post(url, headers=headers, data=data)
+            response = requests.post(url, headers=headers, data=form_data)
             # 判断是否出现状态码异常
             response.raise_for_status()
             if response.json()["data"]:
                 with lock:
                     new_list.append(student_id)
         except requests.exceptions.RequestException as e:
-            print(f'请求出错，请检查网络连接，异常原因:{e}')
+            print(f'请求出错，请检查token，异常原因:{e}')
         q.task_done()
 
 
